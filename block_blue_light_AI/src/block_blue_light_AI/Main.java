08022313539c6dcb6cf3e7d20bfaba898401fbd3
Lee, Seungbeom <vldhtmxk1@gmail.com>
2022-04-24 18:27:19 +0900

    블루라이트 차단율 조절 AI 개발!

---
+++ block_blue_light_AI/src/block_blue_light_AI/Main.java
@@ -0,0 +1,50 @@
+package block_blue_light_AI;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ScreenLight 소속함수
+		int[][] slF = { { -1, 1, 20, 40 }, { 20, 40, 60, 80 }, { 60, 80, 100, 101 } };
+		// UsingTime 소속함수
+		int[][] utF = { { -1, 0, 1, 3 }, { 2, 4, 5, 7 }, { 5, 7, 8, 10 }, { 9, 10, 20, 24 } };
+		//블루라이트 차단율 소속함수
+		int[][] brF = {{0,0,20,20},{20,20,40,40},{40,40,60,60},{60,60,80,80},{80,80,100,100}};
+		
+		Scanner scan = new Scanner(System.in);
+		//화면 밝기 입력
+		System.out.println("화면 밝기 0~100(%)");
+		int screenLight = scan.nextInt();
+		//사용 시간 입력
+		System.out.println("사용 시간 0~24(H)");
+		int usingTime = scan.nextInt();
+		
+	
+		Membership sl = new Membership(screenLight, slF);
+		Membership ut = new Membership(usingTime, utF);
+		
+		//1. Fuzzification
+		sl.doFuzzification();
+		ut.doFuzzification();
+		
+		BlockingRate controlBlockingRate = new BlockingRate(sl.getDegree(), ut.getDegree());
+		
+		//2. Fuzzy Logic Operation
+		controlBlockingRate.doFuzzyLogicOperation();
+		
+		//3. Implication
+		controlBlockingRate.doImplication(brF);
+		
+		//4. Aggregation
+		controlBlockingRate.doAggregation();
+		
+		//5. Defuzzification
+		controlBlockingRate.doDefuzzification();
+		
+		//결과 출력
+		controlBlockingRate.printResult();
+	}
+
+}
---
+++ block_blue_light_AI/src/block_blue_light_AI/Membership.java
@@ -0,0 +1,39 @@
+package block_blue_light_AI;
+
+/*
+ * 소속함수를 통해 소속도를 구하는 클래스
+ */
+public class Membership {
+	private int[][] function; //계산될 소속함수
+	private int x;//입력값
+	private double[] degree; //계산된 소속도 
+	
+	/*입력값과 그에 따른 소속함수를 파라미터로 받아옴*/
+	public Membership(int x, int[][] function) {
+		this.x = x;
+		this.function = function;
+		degree = new double[function.length];
+	}
+	
+	/*	Step 1 : Fuzzification
+	 * 	사다리꼴 형태의 소속함수의 소속도를 구함
+	 */
+	public void doFuzzification() {
+		for (int i = 0,end = function.length; i < end; i++) { // 각 함수에 대해 소속도를 구함
+			if (x < function[i][0]) // x < a
+				degree[i] = 0;
+			else if (x >= function[i][0] && x < function[i][1]) //a <= x < b
+				degree[i] = (x - function[i][0]) / (double)(function[i][1] - function[i][0]);
+			else if (x >= function[i][1] && x < function[i][2]) // b <= x < c
+				degree[i] = 1;
+			else if (x >= function[i][2] && x < function[i][3]) // c <= x <d
+				degree[i] = (function[i][3] - x) / (double)(function[i][3] - function[i][2]);
+			else if (x >= function[i][3]) // d <= x
+				degree[i] = 0;
+		}
+	}
+	/*소속도 getter*/
+	public double[] getDegree() {
+		return degree;
+	}
+}
